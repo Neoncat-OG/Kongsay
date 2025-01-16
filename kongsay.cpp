687bdf18cc022ddba3d1d7319d627adf0eda5dcd
Neon Cat <147309716+Neoncat-OG@users.noreply.github.com>
2025-01-16 11:14:35 +0100

    Add files via upload

---
+++ kongsay.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main(int argc, const char* argv[]) {
+	if(argc == 2) {
+		size_t length = strlen(argv[1]) + 1;
+		char und = '_';
+		char spc = ' ';
+		std::string space(length, spc);
+		std::string line(length, und);
+		std::cout << R"(
+                               )" << line << R"(
+                   /\  ____   /)" << space << R"(\
+                   <> ( oo ) < )" << argv[1] << R"( |
+                   <>_| ^^ |_ \)" << line << R"(/
+                   <>   @    \
+                  /~~\ . . _ |
+                 /~~~~\    | |
+                /~~~~~~\/ _| |
+                |[][][]/ / [m]
+                |[][][[m]
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[][][]|
+                |[|--|]|
+                |[|  |]|
+                ======== 
+               ==========
+               |[[    ]]|
+               ==========)" << "\n";
+	} else {
+		return 1;
+	}
+	return 0;
+}
